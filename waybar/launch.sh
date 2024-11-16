1752667289956c53631090851ba1938a992f1cac
Nova-15 <178902183+Nova-15@users.noreply.github.com>
2024-11-16 15:44:33 +0100

    Improve launch.sh script that launches waybar

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -12,11 +12,27 @@
 # ---------------------------------------------------------------
 
 # ---------------------------------------------------------------
-# Quit running waybar instances
+# Define the folder where Waybar configuration and style files are located
 # ---------------------------------------------------------------
-killall waybar
+WAYBAR_FOLDER="$HOME/.config/waybar/"
+CONFIG_PATH="$WAYBAR_FOLDER/config.jsonc"
+STYLE_PATH="$WAYBAR_FOLDER/style.css"
+# Space-separated string of files to watch
+CONFIG_FILES="$CONFIG_PATH $STYLE_PATH"
 
 # ---------------------------------------------------------------
-# Start waybar in the background
+# Infinite loop to monitor changes and restart Waybar
 # ---------------------------------------------------------------
-waybar &
+while true; do
+    # Start Waybar in the background with specified config and style
+    waybar --config $CONFIG_PATH --style $STYLE_PATH >$WAYBAR_FOLDER/.waybar.err 2>&1 &
+    
+    # Wait for changes in the configuration or style files
+    inotifywait -e create,modify $CONFIG_FILES
+    
+    # Kill any running instances of media-player.py (if applicable)
+    pkill -f media-player.py 2>/dev/null
+    
+    # Terminate the Waybar process
+    killall waybar 2>/dev/null
+done
