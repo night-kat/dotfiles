7d1fefbbc8ead390eba16c862fa8bdd91e9509ec
Nova-15 <178902183+Nova-15@users.noreply.github.com>
2024-11-24 15:21:12 +0100

    Fix moving window focus in nvim.

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -26,9 +26,9 @@ map("n", "k", "<Up>", {desc = "move up(normal mode)"})
 map("n", "l", "<Right>", {desc = "move right(normal mode)"})
 
 map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-j>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-k>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-l>", "<C-w>k", { desc = "switch window up" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
 
